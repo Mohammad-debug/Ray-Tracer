538ec011c861e6e705f3bbe00e4a26479559c3b4
Mohammad-debug <anasmd4u@gmail.com>
2023-09-20 01:48:35 -0400

    playaround

--- main.cpp
+++ main.cpp
@@ -18,11 +18,9 @@ color ray_color(const ray& r, const hittable& world) {
     }
 
     vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.x() + 1.0);
+    auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
-
-
 
 int main() {
     // Image
@@ -40,14 +38,15 @@ int main() {
     hittable_list world;
 
     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-    //world.add(make_shared<sphere>(point3(0,-100.5,-1), 100)); //ground
+    world.add(make_shared<sphere>(point3(1, 0, -1), 0.2));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100)); //ground
 
     // Camera
 
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
     auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
-    auto camera_center = point3(0, 0, 0);
+    auto camera_center = point3(4.1, 3, 7);
 
     // Calculate the vectors across the horizontal and down the vertical viewport edges.
     auto viewport_u = vec3(viewport_width, 0, 0);
@@ -68,10 +67,20 @@ int main() {
     for (int j = 0; j < image_height; ++j) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-            color pixel_color = ray_color(r);
+            color pixel_color(0, 0, 0);
+            for (int sample = 0; sample < samples_per_pixel; ++sample) {
+                // Get ray
+                auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+                double random1 = rand() / (RAND_MAX + 1.0);
+                double random2 = rand() / (RAND_MAX + 1.0);
+                auto pixel_sample_square = ((-0.5 + random1) * pixel_delta_u) + ((-0.5 + random2) * pixel_delta_v);
+                auto pixel_sample = pixel_center + pixel_sample_square;
+                auto ray_direction = pixel_sample - camera_center;
+                ray r(camera_center, ray_direction);
+
+                pixel_color += ray_color(r, world);
+            }
+
 
             //Write color
             static const interval intensity(0.000, 0.999);
